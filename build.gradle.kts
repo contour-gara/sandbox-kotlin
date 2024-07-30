ad4c003274f3bc5f76df30a01981776ed5cbd24c
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-07-30 23:46:34 +0900

    gradle 移行

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val versions by extra {
+    mapOf(
+        "java" to "21",
+        "kotlin" to "1.9.23",
+        "kotest" to "5.9.1",
+        "detekt" to "1.23.6",
+    )
+}
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+}
+
+group = "org.contourgara"
+version = "1.0-SNAPSHOT"
+description = "consoleApp"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${versions["kotlin"]}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${versions["kotlin"]}")
+    testImplementation("io.kotest:kotest-runner-junit5:${versions["kotest"]}")
+    testImplementation("io.kotest:kotest-assertions-core:${versions["kotest"]}")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${versions["detekt"]}")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "${versions["java"]}"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        events = setOf(
+            TestLogEvent.FAILED,
+            TestLogEvent.PASSED,
+            TestLogEvent.SKIPPED,
+            TestLogEvent.STANDARD_OUT
+        )
+
+        exceptionFormat = TestExceptionFormat.FULL
+        showStandardStreams = true
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
+}
+
+detekt {
+    parallel = true
+    ignoreFailures = false
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/config/detekt/detekt.yml")
+    basePath = projectDir.absolutePath
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(false)
+        html.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(true)
+    }
+}
